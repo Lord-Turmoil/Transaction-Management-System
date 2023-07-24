9cd145be441d02b7fbdb2d74f46297a335778c2f
Lord Turmoil <tony-turmoil@qq.com>
2023-07-24 17:30:58 +0800

    update copyright

---
+++ src/main/tms/shared/Errors.java
@@ -0,0 +1,11 @@
+/*******************************************************************************
+ * Copyright (C) 2023 Tony Skywalker. All Rights Reserved
+ *    Filename: Errors.java
+ * Last Update: 7/24/23, 5:08 PM
+ */
+
+package tms.shared;
+
+public class Errors {
+    public static final String IllegalArgumentCount = "Illegal argument count";
+}
